2544366f2aa25316f25acb279e5edc8e7ea7b277
Pipatq <pipatpong3432@gmail.com>
2025-04-20 18:03:31 +0700

    เสดละพี่

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,4 @@
 class AppConfig {
-  static const String baseUrl = "http://192.168.1.36:5000"; // 🔁 Replace with your IP
-  // static const String baseUrl = "http://172.20.10.4:5000"; // 🔁 Replace with your IP
-  // static const String baseUrl = "https://itds283-sec1-group18.onrender.com/"; 
+  static const String baseUrl = "https://your-api-url";
 }
+
